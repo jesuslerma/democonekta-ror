68a6303001a09e459f2cd6b926f6859f1fd41b45
Jesus Lerma <shuyojl@gmail.com>
2014-06-27 14:35:40 -0500

    Add

--- app/controllers/checkouts_controller.rb
+++ app/controllers/checkouts_controller.rb
@@ -10,18 +10,40 @@ class CheckoutsController < ApplicationController
 	    description: "Pizza Delivery at CMPMX",
 	    reference_id: "001-id-cmpmx-jlerma",
 	    card: params[:conektaTokenId] 
-	#"tok_a4Ff0dD2xYZZq82d9"
+		#"tok_a4Ff0dD2xYZZq82d9"
 	  })
-	rescue Conekta::ValidationError => e
-	  puts e.message 
-	#alguno de los parámetros fueron inválidos
-	rescue Conekta::ProcessingError => e
-	  puts e.message 
-	#la tarjeta no pudo ser procesada
-	rescue Conekta::Error
-	  puts e.message 
-	#un error ocurrió que no sucede en el flujo normal de cobros como por ejemplo un auth_key incorrecto
-	end
+	  charge_n = Charge.new(id: @charge.id,
+    	amount:  @charge.amount,
+	    livemode: @charge.livemode,
+	    created_at: @charge.created_at,
+	    status: @charge.status,
+	    currency: @charge.currency,
+	    description: @charge.description,
+	    reference_id: @charge.reference_id,
+	    failure_code: @charge.failure_code,
+	    failure_message: @charge.failure_message,
+	    card_name: @charge.payment_method.name,
+	    card_exp_month: @charge.payment_method.exp_month,
+	    card_exp_year: @charge.payment_method.exp_year,
+	    card_auth_code: @charge.payment_method.auth_code,
+	    card_last4: @charge.payment_method.last4,
+	    card_brand: @charge.payment_method.brand
+		)
+		if charge_n.save
+			render 'checkouts/charge'	
+		else
+			respond_to raise
+		end
+		rescue Conekta::ValidationError => e
+	  	puts e.message 
+			#alguno de los parámetros fueron inválidos
+		rescue Conekta::ProcessingError => e
+	  	puts e.message 
+			#la tarjeta no pudo ser procesada
+		rescue Conekta::Error
+	  	puts e.message 
+		#un error ocurrió que no sucede en el flujo normal de cobros como por ejemplo un auth_key incorrecto
+		end
   end
   def oxxo
   end
